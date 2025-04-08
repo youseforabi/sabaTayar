2a12089d35b30e1bebd61d37be02043565bfd6ff
youseforabi <dipencilcom@gmail.com>
2025-04-08 21:37:55 +0200

    http to https

--- src/environment/environment.ts
+++ src/environment/environment.ts
@@ -1,7 +1,7 @@
 export const environment = {
     production: false,
-    apiUrl: 'http://sabatoursapi.runasp.net/api',
-    apiUlrDashboard:'http://sabatoursapi.runasp.net/api/Dashboard'
+    apiUrl: 'https://sabatoursapi.runasp.net/api',
+    apiUlrDashboard:'https://sabatoursapi.runasp.net/api/Dashboard'
   };
   
   
